b3e2b8edc0d3f6a355b8a9875b8bc2be90e12fc8
drewgillespie1 <49172018+drewgillespie1@users.noreply.github.com>
2019-05-07 13:49:45 -0700

    Update countEvens.cpp

--- countEvens.cpp
+++ countEvens.cpp
@@ -3,5 +3,10 @@
 #include <iostream>
 
 int countEvens(int a[], int size) {
-  return -42; // STUB!  Replace with correct code.
+  count=0;
+  for(int i=0; i<size;i++){
+    if(a[i]%2==0)
+      count+=1;
+  }
+  return count;
 }
