4a551899b7295745cd9e7de8c14eac50be32e835
drewgillespie1 <49172018+drewgillespie1@users.noreply.github.com>
2019-05-07 14:08:10 -0700

    Update countPrimes.cpp

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -5,37 +5,38 @@
 #include "utility.h"
 
 int countPrimes(int a[], int size) {
-  int countprime=0;
+  int countnp=0;
   
   for(int j=0; j<size;j++){
     
     if(a[j]>=2){
       for (int i=2; i<=a[j]/2; i++){
         if (a[j]%i==0){
-          countprime+=0;
+          countnp+=1;
           break;
         
+        }
+        
       }
-        else{countprime+=1;}
-    }
      
-  }
-  if(a[j]<=-2){    
-    for (int i=-2; i<=-a[j]/2; i--){
-      if (a[j]%i==0){
-        countprime+=0;
-        break;
-      }
-      else{countprime+=1;}
     }
-  }
+    else if(a[j]<=-2){    
+      for (int i=-2; i<=-a[j]/2; i--){
+        if (a[j]%i==0){
+          countnp+=1;
+          break;
+        }
+      
+      }
+    }
     
-  if(a[j]==0)
-    countprime+=0;
-  if(a[j]==1||a[j]==-1)
-    countprime+=0;
+    else if(a[j]==0)
+      countnp+=1;
+    else if(a[j]==1||a[j]==-1)
+      countnp+=1;
   
     
-}
+  }
+  int countprime=size-countnp;
   return countprime;
 }
