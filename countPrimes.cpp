f6449ae81931ccf15eef02793a067e2b762b3fb4
drewgillespie1 <49172018+drewgillespie1@users.noreply.github.com>
2019-05-07 13:59:31 -0700

    Update countPrimes.cpp

--- countPrimes.cpp
+++ countPrimes.cpp
@@ -6,6 +6,36 @@
 
 int countPrimes(int a[], int size) {
   int countprime=0;
+  
   for(int i=0, i<size,i++){
     
+    if(a[i]>=2){
+      for (int i=2; i<=x/2; i++){
+        if (a[i]%i==0){
+          countprime+=0
+          break;
+        
+      }
+        else{countprime+=1;}
+    }
+     
+  }
+  if(a[i]<=-2){    
+    for (int i=-2; i<=-x/2; i--){
+      if (a[i]%i==0){
+        countprime+=0;
+        break;
+      }
+      else{countprime+=1;}
+    }
+  }
+    
+  if(a[i]==0)
+    countprime+=0;
+  if(a[i]==1||a[i]==-1)
+    countprime+=0;
+  
+    
+}
+  return countprime;
 }
